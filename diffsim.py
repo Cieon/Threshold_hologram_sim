797ba12c24fba2e09868f0a757cf825a141df8b2
Mateusz Sadowski <62396972+Cieon@users.noreply.github.com>
2022-02-03 13:21:16 +0100

    Descriptions

--- diffsim.py
+++ diffsim.py
@@ -23,8 +23,7 @@ if len(arguments) > 1:
 file_path = 'eight_128px_complex.TAB'  # Default file path, variable holding the path
 sim_names = ['ssampling', 'wavelength [nm]', 'extent_x [mm]', 'extent_y [mm]', 'intensity', 'distance [cm]', 'w0 [um]',
              'threshold', 'amp_mod', 'steps', 'extended', 'verbose']  # Names of parameters of simulation
-sim_var = [16, 632.8, 30, 30, 0.1, 100, 80, 0.5, 1.0, 0, 0,
-           0]  # Default values of simulation parameters in order of sim_names
+sim_var = [16, 632.8, 30, 30, 0.1, 100, 80, 0.5, 1.0, 0, 0, 0]  # Default values of simulation parameters in order of sim_names
 loop_names = ['enabled', 'looping parameter', 'starting value', 'final value', 'step']  # Names of loop parameters
 loop_var = [0, 1, 10, 100, 10]  # Default values of loop parameters in order of loop_names
 loop_mapping = [0, 1, 5, 6, 7, 8, 9, 10]  # Mapping looping parameter selection to correct simulation variable
@@ -43,30 +42,38 @@ if menu is 1:
     # MENU OPERATED RUNTIME
     if int(loop_var[0]) is 0:
         # SINGLE MODE
-
+        # Create a MonochromaticField class instance
         F = MonochromaticField(wavelength=sim_var[1] * nm, extent_x=sim_var[2] * mm, extent_y=sim_var[3] * mm,
                                Nx=size * sim_var[0], Ny=size * sim_var[0], intensity=sim_var[4])
+        # Use the phase_sampling method to write the hologram phase using threshold cells
         F.phase_sampling(Phase, w0=sim_var[6] / 1000 * mm, size=size, sampling=int(sim_var[0]), threshold=sim_var[7],
-                         amp_mod=sim_var[8],
-                         steps=int(sim_var[9]), extended=int(sim_var[10]), verbose=bool(sim_var[11]))
+                         amp_mod=sim_var[8], steps=int(sim_var[9]), extended=int(sim_var[10]), verbose=bool(sim_var[11]))
+        # Apply a spherical wave over the pattern to make it focus at the specific distance
         F.spherical_wave(z0=sim_var[5] * cm)
+        # Integrate the field intensity to calculate diffraction efficiency
         F.integrate_intensity()
 
+        # Propagate the pattern to the source of the spherical wave to reconstruct the image and acquire the plot rgb values
         rgb1 = F.compute_colors_at(sim_var[5] * cm)
+        # Plot the acquired rgb values of the field
         F.plot(rgb1, xlim=[-F.extent_x / 2 * 1000, F.extent_x / 2 * 1000],
                ylim=[-F.extent_y / 2 * 1000, F.extent_y / 2 * 1000])
+        # Conduct the data analysis of the produced field
         data = F.data_analysis(size, 1, export=True)
     elif int(loop_var[0]) is 1:
         # LOOP MODE
 
+        # Date and time for directory and file naming
         date = datetime.date(datetime.now())
         time = datetime.time(datetime.now())
         time = time.strftime('%H-%M-%S')
         try:
+            # Create a directory with today's date
             os.mkdir('./{}'.format(date))
         except OSError as error:
             print('Directory already exists (Ignore)')
 
+        # Open up the statistics file and write to it
         datafile = open('./{}/statistics_{}.txt'.format(date, time), 'w')
         datastring = "{}\t{}\t{}\t{}\n".format('variable', 'diff. efficiency', 'contrast', 'speckle noise')
         datafile.write(datastring)
@@ -83,7 +90,8 @@ if menu is 1:
             F.integrate_intensity()
 
             rgb1 = F.compute_colors_at(sim_var[5] * cm)
-            plotpath = './{}/{}={}_{}.png'.format(date, sim_names[loop_mapping[int(loop_var[1]) - 1]], x, time)
+            # Generate a path to the image with a specific name
+            plotpath = './{}/{}={:.3f}_{}.png'.format(date, sim_names[loop_mapping[int(loop_var[1]) - 1]], x, time)
             F.plot(rgb1, xlim=[-F.extent_x / 2 * 1000, F.extent_x / 2 * 1000],
                    ylim=[-F.extent_y / 2 * 1000, F.extent_y / 2 * 1000], export=plotpath)
             data = F.data_analysis(size, 1, export=True)
@@ -97,6 +105,7 @@ if menu is 1:
     # USAGE EXAMPLE / MANUAL MODE
     # VARIABLES REQUIRED TO BE REPLACED WITH VALUES ARE IN ALL CAPS
     # FOR LOOP IMPLEMENTATION LOOK BELOW
+    # METHOD DESCRIPTIONS ABOVE AND IN MONOCHROMATIC_SIMULATOR.PY
 
     F = MonochromaticField(wavelength= WAVELENGTH * nm, extent_x= SIZE IN X AXIS * mm, extent_y= SIZE IN Y AXIS * mm,
                            Nx=size * SUPERSAMPLING, Ny=size * SUPERSAMPLING, intensity= INTENSITY(0.1 DEFAULT))
@@ -124,7 +133,8 @@ if menu is 1:
     # EXAMPLE OF A LOOP
     # VARIABLES REQUIRED TO BE REPLACED WITH VALUES ARE IN ALL CAPS
     # STARTING, LAST AND STEP VALUES FOR THE LOOP NEED TO BE SET WHILE LOOPED VARIABLE NEEDS TO BE REPLACED BY x
-
+    # METHOD DESCRIPTIONS ABOVE AND IN MONOCHROMATIC_SIMULATOR.PY
+    
     os.mkdir('./loop_results')
     datafile = open('./loop_results/manual_loop_mode.txt', 'w')
     datastring = "{}\t{}\t{}\t{}\n".format('variable', 'diff. efficiency', 'contrast', 'speckle noise')
